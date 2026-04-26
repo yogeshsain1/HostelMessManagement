02ac395a51de5d2ab2a66472b78931834eaacd7f
yogeshsain1 <yogeshsain054@gmail.com>
2026-04-26 22:27:26 +0530

    chore: update next config

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    // Disable cache to avoid Windows/OneDrive file access issues
+    if (isServer) {
+      config.cache = false
+    }
+    return config
+  },
 }
 
 export default nextConfig
